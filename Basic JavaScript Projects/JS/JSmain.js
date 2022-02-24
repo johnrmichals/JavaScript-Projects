b2e6db69dec46009472541d31208ece4f27e07b8
johnrmichals <johnrmichals@gmail.com>
2022-02-24 15:58:23 -0800

    Created JS practice program

---
+++ Basic JavaScript Projects/JS/JSmain.js
@@ -0,0 +1,7 @@
+window.alert("Hello, world!");  //Creates a pop-up
+document.write("Hello, world!"); //Writes text
+var X ="Hello, " //two string variables
+var Y ="world!"
+X.fontcolor("red") //no longer in use
+document.write(X + Y) //prints the two variables
+document.write(5 + 9) //an expression
