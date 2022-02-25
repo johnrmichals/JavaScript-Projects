9bb382299d4b9096b3d38296fbae9f818754dc7a
johnrmichals <johnrmichals@gmail.com>
2022-02-25 14:42:27 -0800

    Created random number generator

---
+++ Basic JavaScript Projects/Project2_functions/JS/JS/main.js
@@ -0,0 +1,4 @@
+function randNum() {
+    document.getElementById("random").innerHTML =
+    Math.Floor(Math.random() * 10)
+}
---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,10 @@
+
+
+function randNum() {
+    document.getElementById("random").innerHTML =
+    Math.floor(Math.random() * 10);
+}
+function add() {
+    document.getElementById("random").innerHTML =
+    document.writeln("hello")
+}
