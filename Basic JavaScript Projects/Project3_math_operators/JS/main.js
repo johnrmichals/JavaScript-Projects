303da6e64836ad74d078b9d3e4e6d273ff45cd08
johnrmichals <johnrmichals@gmail.com>
2022-02-26 18:20:31 -0800

    Added Modular functions to code

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,10 +1,10 @@
-
-
 function randNum() {
-    document.getElementById("random").innerHTML =
-    Math.floor(Math.random() * 10);
-}
-function add() {
-    document.getElementById("random").innerHTML =
-    document.writeln("hello")
+    varX = Math.floor(Math.random() * 10);
+    document.getElementById("random").innerHTML = varX;
+    varA = varX + 1;
+    varB = varX - 1;
+    varC = varX * 2;
+    varD = varX / 2;
+    varE = varX % 2;
+    document.write("your number is " + varX + ". One more than that is " + varA + " and one less is " + varB + ". Doubled, it is " + varC + " and halved it is " + varD + " with a remainder of " + varE + ".");
 }
