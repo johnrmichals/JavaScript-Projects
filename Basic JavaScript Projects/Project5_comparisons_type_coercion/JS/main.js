8e53ae8b24074f9c69f2ef3cb5d17c206fd7f8c3
johnrmichals <johnrmichals@gmail.com>
2022-02-27 16:56:35 -0800

    experimented with comparison operators

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,19 @@
-function myFunction {
-document.write()
+function randNum() {
+    varX = Math.floor(Math.random() * 10);
+    document.getElementById("random").innerHTML = varX;
+}
+function greaterFive() {
+    document.getElementById("grt5").innerHTML = (varX > 5);
+}
+function lessFive() {
+    document.getElementById("less5").innerHTML = (varX < 5 && varX > 2);
+}
+function equalsFive() {
+    document.getElementById("is5").innerHTML = (varX == 5 || varX == 7);
+}
+function notNine() {
+    document.getElementById("not9").innerHTML = !(varX === 9);
+}
+function checkType() {
+    document.write(typeof varX.toString());
 }
