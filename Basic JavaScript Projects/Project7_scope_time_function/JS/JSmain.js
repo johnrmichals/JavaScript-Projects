7502acfca9dec7ca300fe088a821026cb4194132
johnrmichals <johnrmichals@gmail.com>
2022-03-01 13:44:08 -0800

    made elseif timechecker program

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/JSmain.js
@@ -0,0 +1,15 @@
+var time = new Date(); //this is a global variable
+
+function timeCheck() {
+    var sleepytime = 22; //this is a local variable
+    document.getElementById("checktime").innerHTML = "It is " + time;
+    if (time.getHours() < 6 ) {
+        document.getElementById("whatdo").innerHTML = "Time for sleep!";
+    }
+    else if (time.getHours() > 20) {
+        document.getElementById("whatdo").innerHTML = "Get ready for bed!";
+    }
+    else {
+        document.getElementById("whatdo").innerHTML = "Up and at em! Only " + (sleepytime - time.getHours()) + " hours before bed!";
+    }
+}
